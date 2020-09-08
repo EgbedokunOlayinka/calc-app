e75b3f6989a23de396dd251cc07943c0c63ddd95
Egbedokun Olayinka <egbedokunolayinka@gmail.com>
2020-09-08 20:33:19 +0100

    Updates

---
+++ precache-manifest.08f8ed29f1179b7706e0267bd4daaa6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40f983894401730cc3b92d93815b8c68",
+    "url": "/mycalc-app/index.html"
+  },
+  {
+    "revision": "d6c8574e616c18edc8c0",
+    "url": "/mycalc-app/static/css/main.54bcf688.chunk.css"
+  },
+  {
+    "revision": "42101d04e76e75ad6b57",
+    "url": "/mycalc-app/static/js/2.4210bd49.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/mycalc-app/static/js/2.4210bd49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6c8574e616c18edc8c0",
+    "url": "/mycalc-app/static/js/main.65e956dd.chunk.js"
+  },
+  {
+    "revision": "7028c1202c63c459d124",
+    "url": "/mycalc-app/static/js/runtime-main.034c0234.js"
+  }
+]);
